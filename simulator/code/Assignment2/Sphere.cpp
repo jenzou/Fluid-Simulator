085d19c74b5fb165038610f354aa3c1a2a5df04b
Jennifer Zou <jzou98@gmail.com>
2019-05-09 13:54:20 -0700

    screenshots added v1

---
+++ simulator/code/Assignment2/Sphere.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cs184-aci on 5/9/19.
+//
+
+#include "Sphere.h"
---
+++ simulator/code/Assignment2/Sphere.h
@@ -0,0 +1,8 @@
+//
+// Created by cs184-aci on 5/9/19.
+//
+
+#ifndef SIMULATOR_SPHERE_H
+#define SIMULATOR_SPHERE_H
+
+#endif //SIMULATOR_SPHERE_H
